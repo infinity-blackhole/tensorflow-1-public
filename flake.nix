d0dfcd023eb6fc8c9243ddcda7e42157d6def92b
Shikanime Deva <deva.shikanime@protonmail.com>
2022-03-28 23:28:08 +0200

    Add nix

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  description = "Tensorflow 1 Public";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/release-21.11";
+
+  outputs = { self, nixpkgs }: {
+    devShell.x86_64-darwin = import ./shell.nix {
+      pkgs = import nixpkgs {
+        system = "x86_64-darwin";
+        config.allowUnfree = true;
+      };
+    };
+    devShell.x86_64-linux = import ./shell.nix {
+      pkgs = import nixpkgs {
+        system = "x86_64-linux";
+        config.allowUnfree = true;
+      };
+    };
+  };
+}
---
+++ shell.nix
@@ -0,0 +1,12 @@
+{ pkgs ? import <nixpkgs> { }, lib ? pkgs.lib, stdenv ? pkgs.stdenv }:
+
+pkgs.mkShell {
+  buildInputs = [
+    (pkgs.python39.withPackages (pypkgs: with pypkgs; [ pip poetry ]))
+    pkgs.bashInteractive
+  ];
+  shellHook = ''
+    poetry shell
+  '';
+  LD_LIBRARY_PATH = lib.makeLibraryPath [ stdenv.cc.cc.lib ];
+}
